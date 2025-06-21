5c5f52819a012e56ae04c6301c8f511333ce86df
Vijay S <vijay-zstch1395@vijay-zstch1395.csez.zohocorpin.com>
2025-06-21 12:21:07 +0530

    title added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'VJ Music',
+  title: 'VJ Sonic',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
